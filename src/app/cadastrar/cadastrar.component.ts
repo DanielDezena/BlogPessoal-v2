61516f9ef4bdd1da7edd4cea8df9ad754119f696
jo se <danieldezena098@gmail.com>
2021-12-06 23:21:56 -0300

    criação da conexão com o front e backend

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) { 
+    
+  }
+  confirmarSenha: string
+  tipoUsuario: string
 
-  ngOnInit(): void {
+  user: User = new User;
+  ngOnInit() {
+    window.scroll(0,0)
+
+    
   }
 
+  confirmSenha(event:any) {
+    this.confirmarSenha = event.target.value
+  }
+
+    tipoUser(event:any){
+      this.tipoUsuario = event.target.value
+    }
+
+    cadastrar(){
+      this.user.tipo = this.tipoUsuario
+      if(this.user.senha == this.confirmarSenha){
+        this.authService.cadastrar(this.user).subscribe((resp: User) => {
+          this.user = resp
+          this.router.navigate(['/entrar'])
+          alert('conta criada!')
+        })
+      }else{
+        alert('as senhas estão incorretas!')
+      }
+    }
 }
